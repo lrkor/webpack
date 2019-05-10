b17b1420ff873318090d252fbf433f11c77ef1b5
徐文斌 <1055409319@qq.com>
2019-05-10 16:36:24 +0800

    webpack 构建

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import _ from 'lodash';
+import printMe from './print.js';
+
+// import './style.css';
+// import image from './2.png';
+// import Data from './data.xml';
 function component() {
     var element = document.createElement('div');
-
+    var btn = document.createElement('button');
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    // element.classList.add('hello');
+
+    // 将图像添加到我们现有的 div。
+    // var myImage = new Image();
+    // myImage.src = image;
+    // element.appendChild(myImage);
+
+    // console.log(Data);
     return element;
 }
 
 document.body.appendChild(component());
+if (module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,76 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+
+    //加载js
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        // print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Output Management"
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname,'dist')
-    }
+        // filename: 'bundle.js',
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+
+
+    // module: {
+    //     rules: [
+    //         //加载css
+    //         {
+    //             test: /\.css$/,
+    //             use:[
+    //                 'style-loader',
+    //                 'css-loader',
+    //             ]
+    //         },
+    //
+    //         //加载图片
+    //         {
+    //             test: /\.(png|svg|jpg|gif)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //
+    //         //加载字体
+    //         {
+    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
+    //             use: [
+    //                 'file-loader'
+    //             ]
+    //         },
+    //
+    //         //加载数据
+    //         {
+    //             test: /\.(csv|tsv)$/,
+    //             use: [
+    //                 'csv-loader'
+    //             ]
+    //         },
+    //         {
+    //             test: /\.xml$/,
+    //             use: [
+    //                 'xml-loader'
+    //             ]
+    //         },
+    //     ]
+    // }
 };
